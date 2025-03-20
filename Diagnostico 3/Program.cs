d441469996f9398ab76c75f76a660e941232afb7
info-1 <info-1@INFO1-PC1>
2025-03-19 22:20:30 -0300

    Agregar archivos de proyecto.

---
+++ Diagnostico 3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagnostico_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int salida = 0;
+            do
+            {
+                Console.WriteLine("Eliga una opcione el 1 aal 5");
+                string Opciones = Console.ReadLine();
+                switch (Opciones)
+                {
+                    case "1":
+                        string [] canciones1 = new string[5];
+                        int cont = 0;
+                        while(cont<5)
+                        {
+                            Console.WriteLine("Ingrese una cancion");
+                            canciones1[cont] = Console.ReadLine();
+                            cont++;
+                        }
+
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                       string [] canciones = new string[5];
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Console.WriteLine("Ingrese una cancion");
+                            canciones[i] = Console.ReadLine();
+                        }
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Console.WriteLine(canciones[i]);
+                        }
+                        Console.WriteLine();
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    default:
+                        break;
+                }
+            } while (salida!=0);
+        }
+    }
+}
